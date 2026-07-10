b242b4c7c10d6664b1b309a8a7b0207b26960163
Charlyne536 <saratms2012@gmail.com>
2026-07-10 17:41:24 +0200

    Annonce publier

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ console.log("Bienvenue sur Sakuria 🌸");
 
 // Les animations et les fonctionnalités
 // seront ajoutées progressivement.
+
+const publishButton = document.getElementById("publish");
+
+if (publishButton) {
+
+    publishButton.addEventListener("click", function () {
+
+        alert("🎉 Publication créée !");
+
+    });
+
+}
